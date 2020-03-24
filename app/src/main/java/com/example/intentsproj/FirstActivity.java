e4644307e21599cfee22790d5aee0d2de3a12b17
piyumantha perera <41514689+piyumantha-perera@users.noreply.github.com>
2020-03-24 12:14:18 -0700

    cal

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,42 +2,53 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
-
     Button btn;
     EditText n1,n2;
     String s1,s2;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        btn=findViewById(R.id.button);
-        n1=findViewById(R.id.editText);
-        n2=findViewById(R.id.editText2);
+
+        n1 = findViewById(R.id.editText);
+        n2 = findViewById(R.id.editText2);
+        btn = findViewById(R.id.button);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                s1 = n1.getText().toString();
+                s2 = n2.getText().toString();
 
-                s1=n1.getText().toString();
-                s2=n2.getText().toString();
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("Value1",s1);
+                intent.putExtra("Value2",s2);
 
-                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
-                intent.putExtra("value1",s1);
-                intent.putExtra("value2",s2);
                 startActivity(intent);
+
+                Context context = getApplicationContext();
+                CharSequence message = "Button clicked succefully";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.show();
+
+
             }
         });
 
 
     }
 }
+
+
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -2,13 +2,122 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button b1,b2,b3,b4;
+    EditText e1,e2;
+    TextView ans;
+    String val1, val2;
+    Integer a,b;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+
+        b1 = findViewById(R.id.button2);
+        b2 = findViewById(R.id.button3);
+        b3 = findViewById(R.id.button4);
+        b4 = findViewById(R.id.button5);
+
+        ans = findViewById(R.id.textView5);
+        e1 = findViewById(R.id.editText3);
+        e2 = findViewById(R.id.editText4);
+
+        val1 = intent.getStringExtra("Value1");
+        val2 = intent.getStringExtra("Value2");
+
+
+        e1.setText(val1);
+        e2.setText(val2);
+
+        a = Integer.parseInt(val1);
+        b = Integer.parseInt(val2);
+
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText(a+"+"+b + "=" + String.valueOf(a+b));
+
+                Context context = getApplicationContext();
+
+                CharSequence message = "Add two numbers";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText(a+"-"+b +"="+ String.valueOf(a-b));
+
+                Context context = getApplicationContext();
+
+                CharSequence message = "Subtract two numbers";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText(a+"*"+b +"="+ String.valueOf(a*b));
+
+                Context context = getApplicationContext();
+
+                CharSequence message = "Multiply two numbers";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
+
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ans.setText(a+"/"+b +"="+ String.valueOf(a/b));
+
+                Context context = getApplicationContext();
+
+                CharSequence message = "Devide two numbers";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
+            }
+        });
+
+
+
+
+
+
     }
 }
