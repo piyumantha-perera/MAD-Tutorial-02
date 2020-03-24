b271fefd91670f00c808ed868ec0ec0d16862b0c
piyumantha perera <41514689+piyumantha-perera@users.noreply.github.com>
2020-03-24 11:06:52 -0700

    cal

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,13 +2,42 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText n1,n2;
+    String s1,s2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        btn=findViewById(R.id.button);
+        n1=findViewById(R.id.editText);
+        n2=findViewById(R.id.editText2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                s1=n1.getText().toString();
+                s2=n2.getText().toString();
+
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("value1",s1);
+                intent.putExtra("value2",s2);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
